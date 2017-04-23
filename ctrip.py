890fb48bfbe6f1ea6448671e7f261b93f910ad1b
songliantao <songliantao@baidu.com>
2017-04-24 06:51:24 +0800

    first 5 days done

--- ctrip.py
+++ ctrip.py
@@ -5,8 +5,14 @@
 import os
 import time
 import datetime
-from selenium import webdriver
+import sys
 
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.expected_conditions import staleness_of
 #
 from urllib.request import urlopen
 from urllib.error import HTTPError
@@ -14,6 +20,10 @@ from urllib.parse import quote
 from bs4 import BeautifulSoup
 
 import json
+
+#user defined
+from ssroom import *
+import logging
 
 def ss_show_error(msg):
   print(msg)
@@ -84,44 +94,153 @@ def ss_get_url_for_hotel(keyword):
 
   return hotelURL
 
+def ss_get_element_text_by_class(element, class_name):
+  try:
+    item = element.find_element_by_class_name(class_name)
+    if item:
+      return item.text
+    else:
+      return None
+  except:
+    print("no class ".class_name)
+  finally:
+    return None
+
+def ss_get_flag_row(driver):
+  table = driver.find_element_by_id('J_RoomListTbl')
+  row_list = table.find_elements_by_tag_name('tr')
+  count = len(row_list)
+  if count > 2:
+    return row_list[1] #取第二条作为table更新的标志行
+  else:
+    ss_show_error("no flag row")
+
 def ss_get_price_for_date(driver, from_date, to_date):
+  lowest_price = sys.maxsize
+  lowest_bed = 'no'
+  lowest_breakfirst = 'no'
+
+  flag_row = ss_get_flag_row(driver)
+
   from_string = from_date.strftime('%Y-%m-%d')
   to_string = to_date.strftime('%Y-%m-%d')
-  driver.find_element_by_name('cc_txtCheckIn').clear()
-  driver.find_element_by_name('cc_txtCheckIn').send_keys(from_string)
-  driver.find_element_by_name('cc_txtCheckOut').clear()
-  driver.find_element_by_name('cc_txtCheckOut').send_keys(to_string)
-  driver.find_element_by_id('changeBtn').click()
-  time.sleep(5) # 控制间隔时间，等待浏览器反映
+  
+  toBox = driver.find_element_by_name('cc_txtCheckOut')
+  if toBox != None:
+    toBox.clear()
+    toBox.send_keys(to_string)
+  else:
+    ss_show_error('no to box')
+
+  fromBox = driver.find_element_by_name('cc_txtCheckIn')
+  if fromBox != None:
+    fromBox.clear()
+    fromBox.send_keys(from_string)
+  else:
+    ss_show_error('no from box')
+
+  search_button = driver.find_element_by_id('changeBtn')
+  if search_button != None:
+    search_button.click()
+  else:
+    ss_show_error('no search button')
+  
+  #如果存在, 则等待dom刷新
+  if flag_row:
+    try:
+      print('wait for current row change')
+      WebDriverWait(driver, 10).until(staleness_of(flag_row))
+    except:
+      print(flag_row.get_attribute('outerHTML'))
+      ss_show_error('timeout to refresh table')
+
+  print('wait for new table')
+  WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, 'J_RoomListTbl'))
+  )
+  
+  table = driver.find_element_by_id('J_RoomListTbl')
+  row_list = table.find_elements_by_tag_name('tr')
+  count = len(row_list)
+  if count > 0:
+    print("get row ok: %d" % count)
+  else:
+    ss_show_error("no row")
+
+  for row in row_list:
+    try:
+      bed = row.find_element_by_class_name('col3')
+    except:
+      logging.info("\n/////////////////////////" + row.get_attribute('outerHTML'))
+      continue
+
+    try:
+      breakfirst = row.find_element_by_class_name('col4')
+    except:
+      logging.info("\n/////////////////////////" + row.get_attribute('outerHTML'))
+      continue
+
+    try:
+      price = row.find_element_by_class_name('base_price')
+    except:
+      logging.info("\n/////////////////////////" + row.get_attribute('outerHTML'))
+      continue
+
+    bed = bed.text
+    breakfirst = breakfirst.text
+    price = price.text
+    #条件是: 双床, 双早, 找最低价格
+    if bed.find('双') != -1 and breakfirst.find('双') != -1:
+      #print('bed: ' + bed + " breakfirst: " + breakfirst + " price: " + price)
+      price = price.strip()
+      while price.isalnum() == False and len(price) >= 2:
+        price = price[1:]
+
+    if price.isalnum():
+      try:
+        real_price = int(price)
+        if real_price > 0 and real_price < lowest_price:
+          lowest_price = real_price
+          lowest_bed = bed
+          lowest_breakfirst = breakfirst
+      except:
+        #print("invalide price: " + price)
+        pass
+
+  print("from: " + str(from_date) + " to: " + str(to_date) + " bed: " + lowest_bed + " breakfirst: " + lowest_breakfirst + " lowest_price: %d" % lowest_price)
+  return lowest_price
 
 def ss_retrieve_all_price_for_hotel(hotelurl):
-    #driver = webdriver.Firefox()
-    chromedriver = '/usr/local/bin/chromedriver'
-    os.environ['webdriver.chrome.driver'] = chromedriver
-    driver = webdriver.Chrome(chromedriver)
-    driver.get(hotelurl)
-    driver.maximize_window() # 将浏览器最大化显示
-    time.sleep(5) # 控制间隔时间，等待浏览器反映
+  dcap = dict(DesiredCapabilities.PHANTOMJS)
+  dcap["phantomjs.page.settings.userAgent"] = (
+     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:25.0) Gecko/20100101 Firefox/25.0 "
+  )
+  dcap["phantomjs.page.settings.loadImages"] = False
+  driver = webdriver.PhantomJS(executable_path="/usr/local/bin/phantomjs", desired_capabilities=dcap)
+  print(hotelurl)
+  driver.get(hotelurl)
+  try:
+    table = EC.presence_of_element_located((By.ID, 'J_RoomListTbl'))
+    WebDriverWait(driver, 10).until(table)
+  except:
+    if os.path.isfile('./test.png'):
+      os.remove('./test.png')
+    driver.save_screenshot("test.png")
+    ss_show_error('timeout to wait table')
+    driver.quit()
 
-    for i in range(0, 100): #当前开始100天
-      start = datetime.date.today() + datetime.timedelta(days=i)
-      end = start + datetime.timedelta(days=1)
-      print("start: "+start.strftime('%Y-%m-%d')+" end: "+end.strftime('%Y-%m-%d'))
-      #tomorrow_string = tomorrow.strftime('%Y-%m-%d')
-      ss_get_price_for_date(driver, start, end)
+  for i in range(0, 5): #当前开始100天
+    start = datetime.date.today() + datetime.timedelta(days=i)
+    end = start + datetime.timedelta(days=1)
+    #print("start: "+start.strftime('%Y-%m-%d')+" end: "+end.strftime('%Y-%m-%d'))
+    #tomorrow_string = tomorrow.strftime('%Y-%m-%d')
+    ss_get_price_for_date(driver, start, end)
+  driver.quit()
+
+# 通过下面的方式进行简单配置输出方式与日志级别
+if os.path.isfile('./logger.log'):
+  os.remove('./logger.log')
+logging.basicConfig(filename='logger.log', level=logging.INFO)
 
 url = ss_get_url_for_hotel('上海大厦')
-print(url)
-chromedriver = '/usr/local/bin/chromedriver'
-os.environ['webdriver.chrome.driver'] = chromedriver
-driver = webdriver.Chrome(chromedriver)
-driver.get(url)
-driver.maximize_window() # 将浏览器最大化显示
-time.sleep(5) # 控制间隔时间，等待浏览器反映
-start = datetime.date.today() + datetime.timedelta(days=5)
-end = start + datetime.timedelta(days=1)
-ss_get_price_for_date(driver, start, end)
-
-
-
-
+ss_retrieve_all_price_for_hotel(url)
