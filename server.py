15fae23d4e9c265e0c5f1f5dee0cf8cd3e357c6c
songliantao <songliantao@baidu.com>
2017-04-27 13:42:08 +0800

    use chartkick

--- server.py
+++ server.py
@@ -2,12 +2,19 @@
 #-*- coding:utf8 -*-
 
 from flask import Flask, render_template
+import chartkick
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder = chartkick.js())
+app.jinja_env.add_extension("chartkick.ext.charts")
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/chart")
+def chart():
+    data = {'2017-04-23':765, '2017-05-8':891, '2017-07-12':1513}
+    return render_template('chart.html', data = data)
 
 @app.route("/user/<name>")
 def user(name):
