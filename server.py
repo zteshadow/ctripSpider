decdf82cff4741ffa568694cb2aa7f0eedef41b3
songliantao <songliantao@baidu.com>
2017-04-27 19:36:38 +0800

    get data from mysql done

--- server.py
+++ server.py
@@ -3,6 +3,30 @@
 
 from flask import Flask, render_template
 import chartkick
+import pymysql
+
+def get_all_data():
+  all_data = {}
+  db = pymysql.connect(host='127.0.0.1', user='root', passwd='Qwertyui123456')
+  cursor = db.cursor(pymysql.cursors.DictCursor)
+  cursor.execute("SET sql_notes = 0;")
+  cursor.execute("USE ctrip;")
+
+  cursor.execute("SET sql_notes = 0; ")
+  cursor.execute("SET sql_notes = 1; ")
+
+  command = "select id, price from lowest_price_eachday where search_date = '2017-04-27' order by id limit 0, 45;"
+  cursor.execute(command)
+  all_items = cursor.fetchall()
+  for item in all_items:
+    all_data[str(item["id"])] = item["price"]
+    #item_string = "'" + str(item["id"]) + "':" + str(item["price"])
+    #print(item_string)
+    #print(item["id"])
+    #print(item["price"])
+    #all_data.append(item_string)
+
+  return all_data
 
 app = Flask(__name__, static_folder = chartkick.js())
 app.jinja_env.add_extension("chartkick.ext.charts")
@@ -13,7 +37,7 @@ def index():
 
 @app.route("/chart")
 def chart():
-    data = {'2017-04-23':765, '2017-05-8':891, '2017-07-12':1513}
+    data = get_all_data()
     return render_template('chart.html', data = data)
 
 @app.route("/user/<name>")
@@ -22,3 +46,4 @@ def user(name):
 
 if (__name__ == '__main__'):
     app.run(debug=True,port=8764)
+
