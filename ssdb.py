64459d25ad11c2de4576ee727919940395f48e6e
songliantao <songliantao@baidu.com>
2017-05-04 10:52:12 +0800

    add data base module

--- ssdb.py
+++ ssdb.py
@@ -1,2 +1,80 @@
 #!/usr/bin/env python3
 
+import pymysql
+
+class ssdb:
+  def show_error(self, msg):
+    print(msg)
+    exit(1)
+
+  def __init__(self):
+    self.cursor = None
+    self.db = pymysql.connect(host='127.0.0.1', user='root', passwd='Qwertyui123456')
+    if self.db:
+      cursor = self.db.cursor()
+      if cursor:
+        self.cursor = cursor
+
+        #'ctrip' data base
+        cursor.execute("SET sql_notes = 0; ")
+        cursor.execute("create database IF NOT EXISTS ctrip")
+        cursor.execute("USE ctrip")
+
+        #'hotel table'
+        cursor.execute("SET sql_notes = 0; ")
+        cursor.execute("create table IF NOT EXISTS find_hotel (id DATE, search_date DATE, price INT, primary key(id, search_date));")
+        cursor.execute("SET sql_notes = 1; ")
+
+        #'find_flights table'
+        cursor.execute("SET sql_notes = 0; ")
+        command = "create table IF NOT EXISTS find_flight (from_city VARCHAR(20), to_city VARCHAR(20), day DATE, search_date DATE, price INT, primary key(from_city, to_city, day));"
+        cursor.execute(command)
+        cursor.execute("SET sql_notes = 1; ")
+
+        #'city code table'
+        cursor.execute("SET sql_notes = 0; ")
+        cursor.execute("create table IF NOT EXISTS city_list (name VARCHAR(20), code VARCHAR(20), primary key(name));")
+        cursor.execute("SET sql_notes = 1; ")        
+
+      else:
+        show_error("cursor create error")
+    else:
+      show_error("database connect error")
+
+  def __del__(self):
+    if self.cursor:
+      self.cursor.close()
+
+    if self.db:
+      self.db.close()
+
+  #return code of city named 'name'
+  def find_city(self, name):
+    code = None
+    if len(name) > 0:
+      command = "select code from city_list where name = '" + name
+      self.cursor.execute(command)
+      code = self.cursor.fetchone()
+
+    return code
+
+  def add_city(self, name, code):
+    if len(name) > 0 and len(code) > 0:
+      data = "insert into city_list (name, code) values('"
+      data += name
+      data += "', '"
+      data += code
+      data += ");"
+      #print(data)
+      self.cursor.execute(data)
+      self.db.commit()
+
+  def city_list_exist(self):
+      command = "select * from city_list;"
+      self.cursor.execute(command)
+      city_list = self.cursor.fetchall()
+      if len(city_list) > 0:
+        return True
+      else:
+        return False;
+  
