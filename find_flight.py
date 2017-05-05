c84fcc558390eafde4ca390a0217e3ad819382ae
songliantao <songliantao@baidu.com>
2017-05-05 12:07:43 +0800

    add util

--- find_flight.py
+++ find_flight.py
@@ -4,16 +4,27 @@
 
 import datetime
 from ssdata import ssdata
+from ssdriver import ssdriver
+from ssutil import ssutil
 
 #'上海', '三亚', 2017-05-05
 def get_price(driver, from_city, to_city, day):
   url = "http://flights.ctrip.com/booking/" + from_city + "-" + to_city + "-day-1.html?DDate1=";
   url += day.strftime('%Y-%m-%d')
   print(url)
+  
+  driver.get(url)
+  try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "J_flightlist2")))
+  except:
+    
+    ssutil.error('timeout to wait table')
 
 if __name__ == '__main__':
-  driver = None
+  driver = ssdriver()
   bddata = ssdata()
+
   from_city = bddata.find_city('上海')
   to_city = bddata.find_city('哈尔滨')
 
@@ -22,5 +33,5 @@ if __name__ == '__main__':
   count = (day_end - current_date).days
   for i in range(0, count):
     day = datetime.date.today() + datetime.timedelta(days=i)
-    price = get_price(driver, from_city, to_city, day)
+    price = get_price(driver.webdriver(), from_city, to_city, day)
 
---
+++ ssutil.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#-*- coding=utf8 -*-
+
+import sys
+
+class ssutil:
+  def __init__(self):
+    pass
+
+  @staticmethod
+  def error(msg):
+    sys.exit(msg)
